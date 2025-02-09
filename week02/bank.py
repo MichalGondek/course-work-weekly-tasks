c6609f14368ee732a30d66c73499ea40a77b7814
unknown <g00472990>
2025-02-09 21:10:25 +0000

    bank.py

---
+++ week02/bank.py
@@ -0,0 +1,14 @@
+# Week Two Bank.py
+# Program to prompt the user and read in two money amounts  
+# Author Michal Gondek
+
+number = int(input('enter amount1(in cent):'))
+number2 = int(input('enter amount2(in cent):'))
+sumcents = number + number2
+sumeuro = sumcents / 100
+print(f'sum of these is € {sumeuro}')  
+
+
+
+
+
